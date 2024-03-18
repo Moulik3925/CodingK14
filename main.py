0e5e901a7efcc1d99d1140ba12e27ac01ec6355f
abhijain565aj <abhijain565aj@gmail.com>
2024-03-19 04:30:23 +0530

    Added method for lowGunPowder

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import sample_scripts.sample2
 import sample_scripts.sample3
 import Trial
 import neeraje
+import Trial_sexy
 
 if __name__ == "__main__":
-    G = Game((40, 40), Trial, neeraje)
+    # G = Game((40, 40), Trial, neeraje)
+    G = Game((40, 40), Trial, Trial_sexy)
+    # G = Game((40, 40), Trial_sexy, Trial)
     G.run_game()
